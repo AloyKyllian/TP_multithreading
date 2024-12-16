29627cede1961ebbe38c6610c353cbe4ada68de4
AloyKyllian <aloykyllian31520@gmail.com>
2024-12-16 13:47:58 +0100

    ruff

--- manager.py
+++ manager.py
@@ -1,26 +1,23 @@
 from multiprocessing.managers import BaseManager
 from queue import Queue
-ADDRESS = ('127.0.0.1',50000)
-AUTHKEY = b'voila'
+
+ADDRESS = ("127.0.0.1", 50000)
+AUTHKEY = b"voila"
 
 
-class QueueManager(BaseManager): 
+class QueueManager(BaseManager):
     pass
-        
-        
-        
-class QueueClient(BaseManager): 
-    pass
-    
-        
 
-if __name__ == '__main__':
+
+class QueueClient(BaseManager):
+    pass
+
+
+if __name__ == "__main__":
     task_queue = Queue()
     result_queue = Queue()
     m = QueueManager(address=ADDRESS, authkey=AUTHKEY)
-    QueueManager.register('task_queue', callable=lambda:task_queue)
-    QueueManager.register('result_queue', callable=lambda:result_queue)
+    QueueManager.register("task_queue", callable=lambda: task_queue)
+    QueueManager.register("result_queue", callable=lambda: result_queue)
     s = m.get_server()
     s.serve_forever()
-
-    
--- minion.py
+++ minion.py
@@ -1,25 +1,25 @@
 from manager import QueueClient
-from task import Task
 
-ADDRESS = ('127.0.0.1',50000)
-AUTHKEY = b'voila'
+ADDRESS = ("127.0.0.1", 50000)
+AUTHKEY = b"voila"
+
 
 class Minion(QueueClient):
     pass
 
-if __name__ == '__main__':
-    Minion.register('task_queue')
-    Minion.register('result_queue')
+
+if __name__ == "__main__":
+    Minion.register("task_queue")
+    Minion.register("result_queue")
     m = Minion(address=ADDRESS, authkey=AUTHKEY)
     m.connect()
     task_queue = m.task_queue()
     result_queue = m.result_queue()
     while True:
         cpt = 0
-        while (task_queue.qsize() > 0):
+        while task_queue.qsize() > 0:
             task = task_queue.get()
             task.work()
             result_queue.put(task)
             cpt += 1
-            print('the task number '+str(task.identifier)+' is done')
-            
+            print("the task number " + str(task.identifier) + " is done")
